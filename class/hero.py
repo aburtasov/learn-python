06d2fef8c71d339603b8c990d76255beb8061c41
Andrey Burtasov <a.burtasov@yarus.ru>
2022-06-12 20:32:31 +0300

    add main2

--- class/hero.py
+++ class/hero.py
@@ -21,3 +21,19 @@ class Hero:
     def set_health(self,new_health):
         self.health = new_health
         
+#---------------------------------------------------------------------------------------        
+class SuperHero(Hero):
+    """Class to Create SuperHero"""
+    def __init__(self, name, level, race,magic_level):
+        """Initiate our SuperHero"""
+        super().__init__(name,level,race)
+        self.magic_level = magic_level
+        #self.__magic = 100
+        self.magic = 100
+        
+    def make_magic(self):
+        """Use magic"""
+        self.magic = self.magic - 10
+        
+    def show_hero(self):
+        print(self.name,self.level,self.race,self.health,self.magic,self.magic_level)
---
+++ class/main2.py
@@ -0,0 +1,14 @@
+from hero import *
+
+
+myhero = Hero("Jack", 80, "white")
+
+mysuperhero = SuperHero("Man",100,"black",150)
+
+myhero.show_hero()
+
+mysuperhero.show_hero()
+
+mysuperhero.make_magic()
+
+mysuperhero.show_hero()
