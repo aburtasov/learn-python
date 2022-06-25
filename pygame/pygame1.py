b65a266d9ead81d37e3fcf4eab13f51efa38be55
Andrey <andrey.burtasov@yandex.ru>
2022-06-25 18:59:22 +0300

    added pygame

---
+++ pygame/pygame1.py
@@ -0,0 +1,12 @@
+
+import pygame
+
+#import pygame.examples.aliens
+#pygame.examples.aliens.main()
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+
+while True:
+    pygame.display.flip()
